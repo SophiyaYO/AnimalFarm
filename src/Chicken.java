01cc0344f77e59ccb2f3eeb49810201b300ea659
SophYo <sophiya.dimitrova@gmail.com>
2019-07-08 14:46:22 +0300

    finished class with all logic in it

--- src/Chicken.java
+++ src/Chicken.java
@@ -9,32 +9,40 @@ public class Chicken {
 
     private void setName(String name) {
         if (name.length() < 1) {
-            validateNameLength("Name cannot be empty.");
+            validateNameLength();
         }
         this.name = name;
     }
 
     private void setAge(int age) {
         if (age < 0 || age > 15) {
-            validateAge("Age should be between 0 and 15.");
+            validateAge();
         }
         this.age = age;
     }
 
-    private void validateAge(String message) {
-        throw new IllegalArgumentException(message);
+    private void validateAge() {
+        throw new IllegalArgumentException("Age should be between 0 and 15.");
     }
 
     public double productPerDay() {
-
+        return calculateProductPerDay();
     }
 
     private double calculateProductPerDay() {
+        if (age < 6) {
+            return 2;
+        }
 
+        if (age > 11) {
+            return 0.75;
+        }
+
+        return 1;
     }
 
-    private void validateNameLength(String message) {
-        throw  new IllegalArgumentException(message);
+    private void validateNameLength() {
+        throw new IllegalArgumentException("Name cannot be empty.");
     }
 
 

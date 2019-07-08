b2c1c8126bfd8c7d34880ef5639c134f899e5647
SophYo <sophiya.dimitrova@gmail.com>
2019-07-08 13:54:00 +0300

    Basic class Chicken Implements fields, constructor & setName

---
+++ src/Chicken.java
@@ -0,0 +1,16 @@
+public class Chicken {
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
